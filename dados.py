7542b78badf7019bbad17024b576c035cbd7f387
bigisra <elisra.barros@gmail.com>
2026-01-03 16:14:07 -0300

    adicionando_dashboardss

--- dados.py
+++ dados.py
@@ -1,10 +1,3 @@
-import streamlit as st
-import pandas as pd 
-import os
-
-# Título da página
-st.title("Distribuição Percentual da População por Cor ou Raça (1872 – 2000)")
-
 dados = {
     'Anos': [1872, 1890, 1940, 1950, 1960, 1980, 1991, 2000],
     'Branca (%)': [58.0, 44.0, 35.8, 37.5, 38.2, 44.8, 47.5, 44.7],
@@ -25,15 +18,5 @@ for i in range(len(dados['Anos'])):
     # Exibe os dados centralizados e com 1 casa decimal
     print(f"{ano:^6} | {branca:^12.1f} | {negra:^12.1f}")
     
-    df = pd.DataFrame(dados)
-    
-df = pd.DataFrame(dados)
-df.to_html('pop.html', index=False)
-print("Arquivo pop.html gerado com sucesso!")
 
-# Pega o diretório onde o dados.py está guardado
-diretorio_atual = os.path.dirname(os.path.abspath(__file__))
-caminho_final = os.path.join(diretorio_atual, 'pop.html')
 
-df.to_html(caminho_final, index=False)
-print(f"Arquivo salvo em: {caminho_final}")
